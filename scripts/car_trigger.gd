d1f5c4474375a74dd1bf004e181dd442901e7ec0
fimlnwz-afk <yuttana.la@kkumail.com>
2025-10-04 21:42:53 +0700

    fixed jingjingjing

--- scripts/car_trigger.gd
+++ scripts/car_trigger.gd
@@ -3,13 +3,11 @@ extends StaticBody3D
 @onready var alert = $"../alert"
 
 func interact():
-	#GlobalSound.play_sound("res://assets/car-engine-start-44357.mp3")
-	#SceneTransition.chang_scene("res://scenes/ending.tscn")
-	if GlobalInventory.fuel != 1 and GlobalInventory.tire != 4 and GlobalInventory.v8 != 1:
+	if GlobalInventory.fuel == 1 and GlobalInventory.tire == 4 and GlobalInventory.v8 == 1:
+		GlobalSound.play_sound("res://assets/car-engine-start-44357.mp3")
+		SceneTransition.change_scene("res://scenes/ending.tscn")
+	else:
 		$AudioStreamPlayer3D.play()
 		alert.visible = true
 		await get_tree().create_timer(2).timeout
 		alert.visible = false
-	else:
-		GlobalSound.play_sound("res://assets/car-engine-start-44357.mp3")
-		SceneTransition.chang_scene("res://scenes/ending.tscn")
