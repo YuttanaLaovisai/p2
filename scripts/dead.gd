99afa8bb8122459b66c02a88cdf8a70cc174dd44
fimlnwz-afk <yuttana.la@kkumail.com>
2025-10-07 17:06:21 +0700

    bug fixed nidnoy

--- scripts/dead.gd
+++ scripts/dead.gd
@@ -3,4 +3,9 @@ extends Node3D
 func _ready() -> void:
 	SceneTransition.play_anim()
 	await get_tree().create_timer(3).timeout
+	GlobalInventory.attic = 0
+	GlobalInventory.basement = 0
+	GlobalInventory.fuel = 0
+	GlobalInventory.tire = 0
+	GlobalInventory.v8 = 0
 	SceneTransition.chang_scene("res://scenes/start.tscn")
